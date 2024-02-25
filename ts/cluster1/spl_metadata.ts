29914e3038a66da52c7c21181d6197557d214849
Nkama Williams <williamsnkama@gmail.com>
2024-02-25 13:26:48 +0100

    Solana C2

--- ts/cluster1/spl_metadata.ts
+++ ts/cluster1/spl_metadata.ts
@@ -1,48 +1,80 @@
-import wallet from "../wba-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { 
-    createMetadataAccountV3, 
-    CreateMetadataAccountV3InstructionAccounts, 
-    CreateMetadataAccountV3InstructionArgs,
-    DataV2Args
+import wallet from "../wba-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createMetadataAccountV3,
+  CreateMetadataAccountV3InstructionAccounts,
+  CreateMetadataAccountV3InstructionArgs,
+  DataV2Args,
 } from "@metaplex-foundation/mpl-token-metadata";
-import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-foundation/umi";
+import {
+  createSignerFromKeypair,
+  signerIdentity,
+  publicKey,
+} from "@metaplex-foundation/umi";
+import { program } from "@coral-xyz/anchor/dist/cjs/native/system";
+import { PublicKey } from "@solana/web3.js";
+import { fromWeb3JsPublicKey } from "@metaplex-foundation/umi-web3js-adapters";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = new PublicKey("BGNqgQkRjr2DNhfBjKMfLB7CkoE8o4um48a6cZqmyvxs");
+
+//Create metadata PDA
+const program_id = new PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
+const seeds = [Buffer.from("metadata"), program_id.toBuffer(), mint.toBuffer()];
+const [metadata_pda, bump] = PublicKey.findProgramAddressSync(
+  seeds,
+  program_id
+);
 
 // Create a UMI connection
-const umi = createUmi('https://api.devnet.solana.com');
+const umi = createUmi("https://api.devnet.solana.com");
 const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 
 (async () => {
-    try {
-        // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+  try {
+    // Start here
+    let accounts: CreateMetadataAccountV3InstructionAccounts = {
+      metadata: fromWeb3JsPublicKey(metadata_pda),
+      mint: fromWeb3JsPublicKey(mint),
+      mintAuthority: signer,
+    };
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+    let data: DataV2Args = {
+      name: "WalkerWBA",
+      symbol: "WWBA",
+      uri: "",
+      sellerFeeBasisPoints: 20,
+      creators: [
+        {
+          address: keypair.publicKey,
+          verified: true,
+          share: 100,
+        },
+      ],
+      collection: null,
+      uses: null,
+    };
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+    let args: CreateMetadataAccountV3InstructionArgs = {
+      data,
+      isMutable: true,
+      collectionDetails: null,
+    };
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+    let tx = createMetadataAccountV3(umi, {
+      ...accounts,
+      ...args,
+    });
 
-        // let result = await tx.sendAndConfirm(umi).then(r => r.signature.toString());
-        // console.log(result);
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+    let result = await tx
+      .sendAndConfirm(umi)
+      .then((r) => r.signature.toString());
+    console.log(result);
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
 })();
+
+//Metadata: 42,44,95,97,88,140,61,211,97,253,7,213,80,93,200,166,215,66,34,165,101,148,84,161,102,120,62,215,196,78,44,9,187,39,14,123,147,228,48,199,110,51,65,149,186,186,15,92,200,72,36,157,240,85,114,106,81,235,76,189,223,122,42,2
--- ts/cluster1/spl_transfer.ts
+++ ts/cluster1/spl_transfer.ts
@@ -1,6 +1,13 @@
-import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../wba-wallet.json"
+import {
+  Commitment,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+} from "@solana/web3.js";
+import wallet from "../wba-wallet.json";
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
+import { token } from "@coral-xyz/anchor/dist/cjs/utils";
 
 // We're going to import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -10,19 +17,41 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("BGNqgQkRjr2DNhfBjKMfLB7CkoE8o4um48a6cZqmyvxs");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("FAXDZafhXmNBykiqSQtDmT3ZELDiRifzPtq9Yv4zEuvM");
 
 (async () => {
-    try {
-        // Get the token account of the fromWallet address, and if it does not exist, create it
-
-        // Get the token account of the toWallet address, and if it does not exist, create it
-
-        // Transfer the new token to the "toTokenAccount" we just created
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+  try {
+    // Get the token account of the fromWallet address, and if it does not exist, create it
+    const tokenFrom = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      keypair.publicKey,
+      false
+    );
+    // Get the token account of the toWallet address, and if it does not exist, create it
+    const tokenTo = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      to,
+      false
+    );
+    // Transfer the new token to the "toTokenAccount" we just created
+    const result = await transfer(
+      connection,
+      keypair,
+      tokenFrom.address,
+      tokenTo.address,
+      keypair,
+      6n * 1_000_000n
+    );
+    console.log(`Final tx: ${result}`);
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
 })();
+//4hpszxrAvSkSsCZZHsK4Z4USvvVN1CzsMZqvKnG46rmRrSDEEuWsDCE2X5y2H4snRiEDzu5fD1qYmhRGGmMbYpQz
